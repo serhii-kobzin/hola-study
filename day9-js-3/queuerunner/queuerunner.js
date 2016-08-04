9273232f671e623c3d8b256d13cd9843e3d8b37a
Serhii <serhii.kobzin@gmail.com>
2016-08-04 18:08:57 +0300

    day 9 - minor fixes

--- day9-js-3/queuerunner/queuerunner.js
+++ day9-js-3/queuerunner/queuerunner.js
@@ -1,32 +1,35 @@
 function QueueRunner(exec) {
-    var queue = [];
+    this.queue = [];
+    this.isPaused = false;
+    this.isRunning = false;
 
     this.onfinish = function (err) {
-        var currentTask = queue.shift();
+        var currentTask = this.queue.shift();
         currentTask.onFinish(err);
-        if (queue.length > 0) {
-            exec(task.data, this.onfinish);
+        if (this.queue.length != 0) {
+            exec(this.queue[0].data, this.onfinish);
         }
-    };
+    }.bind(this);
 
     this.push = function (task) {
-        queue.push(task);
-        if (queue.length == 1) {
+        this.queue.push(task);
+        if (!this.isRunning) {
+            this.isRunning = true;
             exec(task.data, this.onfinish);
         }
-    };
+    }.bind(this);
 
     this.pause = function () {
-        
-    };
+        this.isPaused = true;
+    }.bind(this);
     
     this.resume = function () {
-        
-    };
+        this.isPaused = false;
+    }.bind(this);
     
     this.cleanup = function () {
-
-    };
+        this.isRunning = false;
+    }.bind(this);
 }
 
 
@@ -41,11 +44,13 @@ function QueueRunner(exec) {
 var qr = new QueueRunner(function exec(data, onfinish){
     setTimeout(onfinish, data);
 });
-qr.push({data: 1000, onFinish: function (err) {
-    if (err) {
-        console.log('error!');
-    }  else {
-        console.log('ok!');
-    }
-}});
+
+var callback = function (err) {
+    console.log((err) ? err : 'ok!');
+}
+
+qr.push({data: 1000, onFinish: callback});
+qr.push({data: 2000, onFinish: callback});
+qr.push({data: 500, onFinish: callback});
+
 console.log('some text');
